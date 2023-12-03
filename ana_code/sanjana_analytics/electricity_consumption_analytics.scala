01e83faef48f44168f991da44af0d03cf7a3877f
Sanjana Vijayakumar Nambiar <85699181+Sanjana-Nambiar@users.noreply.github.com>
2023-12-03 21:22:29 +0400

    Update electricity_consumption_analytics.scala
    printing values

--- ana_code/sanjana_analytics/electricity_consumption_analytics.scala
+++ ana_code/sanjana_analytics/electricity_consumption_analytics.scala
@@ -32,6 +32,10 @@ val statsDemandForecast = calculateStats(data, "Demand Forecast (MW)_imputed")
 val statsDemand = calculateStats(data, "Demand (MW)_imputed")
 val statsNetGeneration = calculateStats(data, "Net Generation (MW)_imputed")
 
+println(statsDemandForecast)
+println(statsDemand)
+println(statsNetGeneration)
+
 //Agg by region and Year/ Month / Hour
 val result1 = data.groupBy("Year", "Region").agg(avg("Demand Forecast (MW)_imputed"), avg("Demand (MW)_imputed"), avg("Net Generation (MW)_imputed")).orderBy(col("Region"), col("Year").desc)
 val result2 = data.groupBy("MonthYear", "Region").agg(avg("Demand Forecast (MW)_imputed"), avg("Demand (MW)_imputed"), avg("Net Generation (MW)_imputed")).orderBy(col("Region"), col("MonthYear").desc)
@@ -40,8 +44,13 @@ val result3 = data.groupBy("Hour Number", "Region").agg(avg("Demand Forecast (MW
 result2.coalesce(1).write.option("header", "true").mode("overwrite").csv("final/avg_month")
 result3.coalesce(1).write.option("header", "true").mode("overwrite").csv("final/avg_hour")
 
+result1.show()
+result2.show()
+result3.show()
+
 //analytics to know the growth rate
 val demandWindowSpec = Window.partitionBy("Region").orderBy("Year")
 val demandWithGrowthRates = result1.withColumn("prev_avg_demand", lag("avg(Demand (MW)_imputed)", 1).over(demandWindowSpec)).withColumn("demand_growth", ((col("avg(Demand (MW)_imputed)") - col("prev_avg_demand")) / col("prev_avg_demand")) * 100).withColumn("prev_avg_net", lag("avg(Net Generation (MW)_imputed)", 1).over(demandWindowSpec)).withColumn("net_growth", ((col("avg(Net Generation (MW)_imputed)") - col("prev_avg_net")) / col("prev_avg_net")) * 100).na.fill(0).drop("prev_avg_net", "prev_avg_demand")
 
+demandWithGrowthRates.show()
 demandWithGrowthRates.coalesce(1).write.option("header", "true").mode("overwrite").csv("final/avg_year")
