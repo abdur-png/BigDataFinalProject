9961d2c0e983d702e3dd404a742f8140eed4b1ad
Sanjana Vijayakumar Nambiar <85699181+Sanjana-Nambiar@users.noreply.github.com>
2023-12-03 20:20:12 +0400

    Update electricity_consumption_cleaning.scala
    separating cleaning code

--- etl_code/sanjana_cleaning/electricity_consumption_cleaning.scala
+++ etl_code/sanjana_cleaning/electricity_consumption_cleaning.scala
@@ -3,12 +3,6 @@ import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.types.DoubleType
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.regression.LinearRegression
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.Imputer
-import org.apache.spark.sql.expressions.Window
 
 //function to clean data
 def cleanAndConsolidate(filePath: String): DataFrame = {
@@ -71,53 +65,5 @@ val finalResultWithHeader = finalResult.toDF(header: _*)
 // Save the final result with a header as a single CSV file
 finalResultWithHeader.coalesce(1).write.option("header", "true").mode("overwrite").csv("final/cleanedData")
 
-//saving all the values as type double 
-var cleandf = finalResultWithHeader.withColumn("Demand Forecast (MW)", col("Demand Forecast (MW)").cast(DoubleType)).withColumn("Demand (MW)", col("Demand (MW)").cast(DoubleType)).withColumn("Net Generation (MW)", col("Net Generation (MW)").cast(DoubleType))
-//checking the amount of null values in the respective columns
-val emptyValuesCount = cleandf
-  .select(
-    count(when(col("Demand Forecast (MW)").isNull || isnan(col("Demand Forecast (MW)")), true)).alias("Empty_Demand_Forecast"),
-    count(when(col("Demand (MW)").isNull || isnan(col("Demand (MW)")), true)).alias("Empty_Demand"),
-    count(when(col("Net Generation (MW)").isNull || isnan(col("Net Generation (MW)")), true)).alias("Empty_Net_Generation")
-  )
-  .show()
 
-//looking at the correlation between net generation and demand/demand forecast
-val correlationMatrix = cleandf
-  .select(
-    corr("Demand Forecast (MW)", "Net Generation (MW)").alias("Corr_Demand_Forecast_Net_Generation"),
-    corr("Demand (MW)", "Net Generation (MW)").alias("Corr_Demand_Net_Generation")
-  )
-  .show()
 
-//--------------------TRYING TO IMPUTE DATA 1------------------------ 
-//METHOD 1: Trying to build an linear regression model to impute the values
-//choosing net generation as the feature and demand as the lable 
-//since the number of null values of net gen is very minimal, we drop that for analysis
-val completeData = cleandf.na.drop("all", Seq("Net Generation (MW)")) 
-
-//train test split for linear regression model 
-val trainingData = completeData.filter(col("Demand (MW)").isNotNull)
-val testData = completeData.filter(col("Demand (MW)").isNull)
-
-//building the model and setting the feature and label for the model
-val assembler = new VectorAssembler().setInputCols(Array("Net Generation (MW)")).setOutputCol("features") //creating this as the model only takes in vectors
-val model = new LinearRegression().setFeaturesCol("features").setLabelCol("Demand (MW)").fit(assembler.transform(trainingData))
-
-// Evaluate the model on the training set itself for accuracy 
-val predictions = model.transform(assembler.transform(trainingData))
-
-// Evaluate the model on the training set for accuracy
-val evaluator = new RegressionEvaluator().setLabelCol("Demand (MW)").setPredictionCol("prediction").setMetricName("rmse") 
-val rmse = evaluator.evaluate(predictions)
-println(s"Root Mean Squared Error (RMSE) on training data: $rmse")
-//As the correlation actual showed to us that the correlation is very low so the accuracy of the model is also low
-//would have to look into more sofisticated models so we just imputed the values by the mean
-
-//METHOD 2: Mean value imputation
-val columnsToImpute = Array("Demand Forecast (MW)", "Demand (MW)", "Net Generation (MW)")
-val imputer = new Imputer().setInputCols(columnsToImpute).setOutputCols(columnsToImpute.map(col => s"${col}_imputed")).setStrategy("mean")
-val imputedData = imputer.fit(cleandf).transform(cleandf)
-imputedData.show()
-
-imputedData.coalesce(1).write.option("header", "true").mode("overwrite").csv("final/imputedClean")
